7240ebb70cb94ccb96cce280b257976e170c393e
Mike haslam <ongomobile@gmail.com>
2024-07-06 16:36:20 -0700

    Initial Commit

---
+++ MyCustomers/MyCustomersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomersApp.swift
+//  MyCustomers
+//
+//  Created by Emorn on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCustomersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
